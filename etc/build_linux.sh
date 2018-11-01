f3ebf7592a89f985f2fb61fc19df8c9b58dd2c77
Joseph Ciurej <churayy@gmail.com>
2018-10-31 23:59:00 -0700

    * Added support for automatically building in parallel with max machine cores on Linux.

--- etc/build_linux.sh
+++ etc/build_linux.sh
@@ -3,9 +3,12 @@
 # This script launches 'cmake' to build the project for Linux platforms.
 
 BUILD_TYPE=${1:-Debug}
+BUILD_TASKS=$(lscpu | sed -r -n 's/(^CPU[(]s[)]:.*([0-9]+).*$)/\2/p')
 
 SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 PROJ_PATH=$(dirname ${SCRIPT_PATH})
+
+echo ${BUILD_CPU_DATA}
 
 pushd .
 cd ${PROJ_PATH}
@@ -15,7 +18,7 @@ mkdir -p build
 cd build
 
 cmake -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -DCMAKE_BUILD_TYPE=${BUILD_TYPE} ${PROJ_PATH}
-make && make install
+make -j${BUILD_TASKS} && make -j${BUILD_TASKS} install
 
 if [[ ! -f ${PROJ_PATH}/hmp.out ]]; then
     ln -s ${PROJ_PATH}/build/install/hmp.out ${PROJ_PATH}/hmp.out
