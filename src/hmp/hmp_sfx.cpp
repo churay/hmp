4563ef0dc8272e5f552879b19ab1990d1a80396d
Joseph Ciurej <churayy@gmail.com>
2019-07-12 21:52:10 -0700

    * Fleshed out the interface for the 'hmp::sfx' namespace.

--- src/hmp/hmp_sfx.cpp
+++ src/hmp/hmp_sfx.cpp
@@ -4,13 +4,25 @@ namespace hmp {
 
 namespace sfx {
 
+/// 'hmp::sfx::waveform_t' Functions ///
+
+waveform_t::waveform_t( const wave_f pWaveFun, const float64_t pWavelength, const float64_t pAmplitude, const float64_t pPhase ) :
+        mWaveFun( pWaveFun ), mWavelength( pWavelength ), mAmplitude( pAmplitude ), mPhase( pPhase ) {
+    
+}
+
+
+float64_t waveform_t::operator()( const float64_t pTime ) const {
+    return mWaveFun( pTime, mWavelength, mAmplitude, mPhase );
+}
+
 /// 'hmp::sfx::synth_t' Functions ///
 
 synth_t::synth_t( const bool32_t pRunning ) {
     for( uint32_t pWaveIdx = 0; pWaveIdx < MAX_CHANNEL_COUNT; pWaveIdx++ ) {
         mWaveforms[pWaveIdx] = nullptr;
-        mWaveformPositions[pWaveIdx] = 0.0f;
-        mWaveformDurations[pWaveIdx] = 0.0f;
+        mWaveformPositions[pWaveIdx] = 0.0;
+        mWaveformDurations[pWaveIdx] = 0.0;
     }
 
     mRunning = pRunning;
@@ -18,7 +30,17 @@ synth_t::synth_t( const bool32_t pRunning ) {
 
 
 void synth_t::update( const float64_t pDT ) {
-    // TODO(JRC)
+    for( uint32_t pWaveIdx = 0; pWaveIdx < MAX_CHANNEL_COUNT; pWaveIdx++ ) {
+        if( mWaveformPositions[pWaveIdx] > mWaveformDurations[pWaveIdx] ) {
+            mWaveforms[pWaveIdx] = nullptr;
+            mWaveformPositions[pWaveIdx] = 0.0;
+            mWaveformDurations[pWaveIdx] = 0.0;
+        } else {
+            mWaveformPositions[pWaveIdx] += pDT;
+        }
+    }
+
+    mUpdateDT = pDT;
 }
 
 
@@ -27,13 +49,40 @@ void synth_t::render() {
 }
 
 
-void synth_t::play( const wave_f pWaveform, const float64_t pWaveDuration ) {
-    // TODO(JRC)
+void synth_t::play( const waveform_t* pWaveform, const float64_t pWaveDuration ) {
+    for( uint32_t pWaveIdx = 0; pWaveIdx < MAX_CHANNEL_COUNT; pWaveIdx++ ) {
+        if( mWaveforms[pWaveIdx] == nullptr ) {
+            mWaveforms[pWaveIdx] = pWaveform;
+            mWaveformPositions[pWaveIdx] = 0.0;
+            mWaveformDurations[pWaveIdx] = pWaveDuration;
+        }
+    }
 }
 
 
 void synth_t::toggle() {
     mRunning = !mRunning;
+}
+
+/// 'hmp::sfx::wave' Functions ///
+
+float64_t wave::square( const float64_t pTime, const float64_t pWavelength, const float64_t pAmplitude, const float64_t pPhase ) {
+    return 0.0;
+}
+
+
+float64_t wave::sine( const float64_t pTime, const float64_t pWavelength, const float64_t pAmplitude, const float64_t pPhase ) {
+    return 0.0;
+}
+
+
+float64_t wave::triangle( const float64_t pTime, const float64_t pWavelength, const float64_t pAmplitude, const float64_t pPhase ) {
+    return 0.0;
+}
+
+
+float64_t wave::sawtooth( const float64_t pTime, const float64_t pWavelength, const float64_t pAmplitude, const float64_t pPhase ) {
+    return 0.0;
 }
 
 };
--- src/hmp/hmp_sfx.h
+++ src/hmp/hmp_sfx.h
@@ -1,29 +1,49 @@
 #ifndef HMP_SFX_H
 #define HMP_SFX_H
 
+#include <SDL2/SDL.h>
+
 #include "hmp_consts.h"
 
 namespace hmp {
 
 namespace sfx {
 
-constexpr static uint32_t MAX_CHANNEL_COUNT = 4;
+typedef float64_t (*wave_f)( const float64_t, const float64_t, const float64_t, const float64_t );
 
-typedef float64_t (*wave_f)( float64_t );
+struct waveform_t {
+    waveform_t( const wave_f pWaveFun, const float64_t pWavelength, const float64_t pAmplitude, const float64_t pPhase );
+
+    float64_t operator()( const float64_t pTime ) const;
+
+    wave_f mWaveFun;
+    float64_t mWavelength, mAmplitude, mPhase;
+};
 
 struct synth_t {
+    constexpr static uint32_t MAX_CHANNEL_COUNT = 4;
+
     synth_t( const bool32_t pRunning = true );
 
     void update( const float64_t pDT );
     void render();
 
-    void play( const wave_f pWaveform, const float64_t pWaveDuration );
+    void play( const waveform_t* pWaveform, const float64_t pWaveDuration );
     void toggle();
 
-    wave_f mWaveforms[MAX_CHANNEL_COUNT];
+    const waveform_t* mWaveforms[MAX_CHANNEL_COUNT];
     float64_t mWaveformPositions[MAX_CHANNEL_COUNT];
     float64_t mWaveformDurations[MAX_CHANNEL_COUNT];
+
     bool32_t mRunning;
+    float64_t mUpdateDT;
+};
+
+namespace wave {
+    float64_t square( const float64_t pTime, const float64_t pWavelength, const float64_t pAmplitude, const float64_t pPhase );
+    float64_t sine( const float64_t pTime, const float64_t pWavelength, const float64_t pAmplitude, const float64_t pPhase );
+    float64_t triangle( const float64_t pTime, const float64_t pWavelength, const float64_t pAmplitude, const float64_t pPhase );
+    float64_t sawtooth( const float64_t pTime, const float64_t pWavelength, const float64_t pAmplitude, const float64_t pPhase );
 };
 
 };
