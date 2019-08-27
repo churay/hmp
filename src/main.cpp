0d5a02ce98fd512925ee9bf7bbd53bce5eb3e15e
Joseph Ciurej <churayy@gmail.com>
2019-08-26 20:30:52 -0700

    * (#58) Organized the code to eliminate an issue with rendering artifacts on debug text in step mode.

--- src/main.cpp
+++ src/main.cpp
@@ -399,7 +399,7 @@ int32_t main( const int32_t pArgCount, const char8_t* pArgs[] ) {
 
     /// Update/Render Loop ///
 
-    bool32_t isRunning = true, isStepping = false, doStep = true;
+    bool32_t isRunning = true, isStepping = false, doStep = !isStepping;
 
     bool32_t isRecording = false, isReplaying = false;
     uint32_t currSlotIdx = 0, recSlotIdx = 0;
@@ -566,7 +566,18 @@ int32_t main( const int32_t pArgCount, const char8_t* pArgs[] ) {
         }
 #endif
 
+        glViewport( 0, 0, windowWidth, windowHeight );
+        glMatrixMode( GL_PROJECTION );
+        glLoadIdentity();
+        glOrtho( -1.0f, +1.0f, -1.0f, +1.0f, -1.0f, +1.0f );
+        glMatrixMode( GL_MODELVIEW );
+        glLoadIdentity();
+
+        std::memset( audioBuffer, 0, sizeof(audioBuffer) );
+
         if( doStep ) {
+            // NOTE(JRC): Consider placing these step clauses above with more
+            // functionally similar code (e.g. input reading).
             llce::input::readKeyboard( simInput->keyboard );
 #if LLCE_DEBUG
             if( isRecording ) {
@@ -585,50 +596,41 @@ int32_t main( const int32_t pArgCount, const char8_t* pArgs[] ) {
             }
 #endif
 
-            glViewport( 0, 0, windowWidth, windowHeight );
-            glMatrixMode( GL_PROJECTION );
-            glLoadIdentity();
-            glOrtho( -1.0f, +1.0f, -1.0f, +1.0f, -1.0f, +1.0f );
-            glMatrixMode( GL_MODELVIEW );
-            glLoadIdentity();
-
-            std::memset( audioBuffer, 0, sizeof(audioBuffer) );
-
             isRunning &= dllUpdate( simState, simInput, simDT );
             isRunning &= dllRender( simState, simInput, simOutput );
+        }
 
-            glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
-            glPushMatrix(); {
-                const float32_t viewRatio = ( windowHeight + 0.0f ) / ( windowWidth + 0.0f );
-                glm::mat4 matWorldView( 1.0f );
-                matWorldView *= glm::translate( glm::mat4(1.0f), glm::vec3(-1.0f, -1.0f, 0.0f) );
-                matWorldView *= glm::scale( glm::mat4(1.0f), glm::vec3(2.0f, 2.0f, 1.0f) );
-                matWorldView *= glm::translate( glm::mat4(1.0f), glm::vec3((1.0f-viewRatio)/2.0f, 0.0f, 0.0f) );
-                matWorldView *= glm::scale( glm::mat4(1.0f), glm::vec3(viewRatio, 1.0f, 1.0f) );
-                glMultMatrixf( &matWorldView[0][0] );
+        glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+        glPushMatrix(); {
+            const float32_t viewRatio = ( windowHeight + 0.0f ) / ( windowWidth + 0.0f );
+            glm::mat4 matWorldView( 1.0f );
+            matWorldView *= glm::translate( glm::mat4(1.0f), glm::vec3(-1.0f, -1.0f, 0.0f) );
+            matWorldView *= glm::scale( glm::mat4(1.0f), glm::vec3(2.0f, 2.0f, 1.0f) );
+            matWorldView *= glm::translate( glm::mat4(1.0f), glm::vec3((1.0f-viewRatio)/2.0f, 0.0f, 0.0f) );
+            matWorldView *= glm::scale( glm::mat4(1.0f), glm::vec3(viewRatio, 1.0f, 1.0f) );
+            glMultMatrixf( &matWorldView[0][0] );
 
-                glEnable( GL_TEXTURE_2D ); {
-                    // NOTE(JRC): This is required to get the expected/correct texture color,
-                    // but it's unclear as to why. OpenGL may perform color mixing by default?
-                    glColor4ubv( (uint8_t*)&csWhiteColor );
-                    glBindTexture( GL_TEXTURE_2D, simOutput->gfxBufferCBOs[hmp::GFX_BUFFER_MASTER] );
-                    glBegin( GL_QUADS ); {
-                        glTexCoord2f( 0.0f, 0.0f ); glVertex2f( 0.0f, 0.0f );
-                        glTexCoord2f( 0.0f, 1.0f ); glVertex2f( 0.0f, 1.0f );
-                        glTexCoord2f( 1.0f, 1.0f ); glVertex2f( 1.0f, 1.0f );
-                        glTexCoord2f( 1.0f, 0.0f ); glVertex2f( 1.0f, 0.0f );
-                    } glEnd();
-                    glBindTexture( GL_TEXTURE_2D, 0 );
-                } glDisable( GL_TEXTURE_2D );
-            } glPopMatrix();
+            glEnable( GL_TEXTURE_2D ); {
+                // NOTE(JRC): This is required to get the expected/correct texture color,
+                // but it's unclear as to why. OpenGL may perform color mixing by default?
+                glColor4ubv( (uint8_t*)&csWhiteColor );
+                glBindTexture( GL_TEXTURE_2D, simOutput->gfxBufferCBOs[hmp::GFX_BUFFER_MASTER] );
+                glBegin( GL_QUADS ); {
+                    glTexCoord2f( 0.0f, 0.0f ); glVertex2f( 0.0f, 0.0f );
+                    glTexCoord2f( 0.0f, 1.0f ); glVertex2f( 0.0f, 1.0f );
+                    glTexCoord2f( 1.0f, 1.0f ); glVertex2f( 1.0f, 1.0f );
+                    glTexCoord2f( 1.0f, 0.0f ); glVertex2f( 1.0f, 0.0f );
+                } glEnd();
+                glBindTexture( GL_TEXTURE_2D, 0 );
+            } glDisable( GL_TEXTURE_2D );
+        } glPopMatrix();
 
-            // TODO(JRC): Improve accounting for currently buffered sound data. There
-            // shouldn't be any since we fill the entire buffer every frame, but lag
-            // and backfill may occur in high memory/compute load situations.
-            if( simOutput->sfxDirtyBits[hmp::SFX_BUFFER_MASTER] && !cIsSimulating ) {
-                SDL_QueueAudio( audioDeviceID, &audioBuffer[0], sizeof(audioBuffer) );
-                simOutput->sfxDirtyBits[hmp::SFX_BUFFER_MASTER] = false;
-            }
+        // TODO(JRC): Improve accounting for currently buffered sound data. There
+        // shouldn't be any since we fill the entire buffer every frame, but lag
+        // and backfill may occur in high memory/compute load situations.
+        if( simOutput->sfxDirtyBits[hmp::SFX_BUFFER_MASTER] && !cIsSimulating ) {
+            SDL_QueueAudio( audioDeviceID, &audioBuffer[0], sizeof(audioBuffer) );
+            simOutput->sfxDirtyBits[hmp::SFX_BUFFER_MASTER] = false;
         }
 
 #if LLCE_DEBUG
@@ -729,6 +731,7 @@ int32_t main( const int32_t pArgCount, const char8_t* pArgs[] ) {
         simTimer.wait( cIsSimulating ? 0.0 : -1.0 );
         simDT = simTimer.ft( llce::timer_t::time_e::ideal );
         simFrame += 1;
+
         doStep = !isStepping;
     }
 
