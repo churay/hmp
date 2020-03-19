890037c3b3c818eb1224766a5ed6bf6dfeacf6c6
Joseph Ciurej <churayy@gmail.com>
2020-03-18 21:20:36 -0700

    * Fixed a minor bug with the 'llce::interval_t' average value constructor.

--- src/util/interval_t.cpp
+++ src/util/interval_t.cpp
@@ -20,7 +20,7 @@ interval_t::interval_t( const float32_t pValue1, const float32_t pValue2,
         mMax = std::max( pValue1, pValue2 );
     } else if( pAnchor == interval_t::anchor_e::avg ) {
         mMin = pValue1 - pValue2 / 2.0f;
-        mMax = pValue2 + pValue2 / 2.0f;
+        mMax = pValue1 + pValue2 / 2.0f;
     }
 }
 
