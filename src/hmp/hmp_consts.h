ed934b85f0e30cebb46803891798aa832fa211e3
Joseph Ciurej <churayy@gmail.com>
2020-06-09 21:47:08 -0700

    * Added the 'LLCE_ENUM' macro to help reduce enumeration declaration boilerplate.

--- src/hmp/hmp_consts.h
+++ src/hmp/hmp_consts.h
@@ -5,14 +5,10 @@
 
 namespace hmp {
 
-namespace action { enum action_e { unbound = 0, lup, ldn, llt, lrt, rup, rdn, rlt, rrt, etc, _length }; };
-typedef action::action_e action_e;
+LLCE_ACTION_ENUM( lup, ldn, llt, lrt, rup, rdn, rlt, rrt, etc );
 
-namespace mode { enum mode_e { boot_id = -1, exit_id = -2, game_id = 0, title_id, reset_id }; };
-typedef mode::mode_e mode_e;
-
-namespace team { enum team_e { west, east, neutral }; };
-typedef team::team_e team_e;
+LLCE_ENUM( mode, boot_id = -1, exit_id = -2, game_id = 0, title_id, reset_id );
+LLCE_ENUM( team, west, east, neutral );
 
 constexpr static int8_t WINNING_SCORE = 3;
 
